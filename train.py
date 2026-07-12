5a18bb580ee97aba60c0aee4c381c5c3e2cbe6ca
rutwikroutu <iamrutwik6@gmail.com>
2026-07-12 00:54:13 -0400

    fix: changed ds name

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def get_or_build_tokenizer(config, ds, lang):
     return tokenizer
 
 def get_ds(config):
-    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
+    ds_raw = load_dataset('Helsinki-NLP/opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
 
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
     tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
